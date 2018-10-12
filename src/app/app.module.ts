51ef5ce1aee6c4e857ab7c5a8f0930a7165dd5ec
karasev-a <karasev.avk@gmail.com>
2018-10-12 09:24:46 +0300

    added custom material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { CustomMaterialModule } from './core/material.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +14,8 @@ import {HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
     HttpClientModule,
     // AppRoutingModule,
   ],
