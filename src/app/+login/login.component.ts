4148ec5e193544bc241289bdd5472ff3edc5b2dc
karasev-a <karasev.avk@gmail.com>
2018-10-14 23:12:34 +0300

    deleted nested form in component and template

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material'
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material";
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"],
 })
 
 export class LoginComponent implements OnInit {
     public loginForm: FormGroup = new FormGroup({
-        email: new FormControl(``, [Validators.required, Validators.email, Validators.maxLength(255)]),
-        password: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+        email: new FormControl("", [Validators.required, Validators.email, Validators.maxLength(255)]),
+        password: new FormControl("", [Validators.required, Validators.maxLength(255)]),
       });
     constructor(private router: Router) { }
 
-    ngOnInit() {
+    public ngOnInit() {
     }
-    
-    login(): void {
-        
+
+    public login(): void {
+
     }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,55 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, FormBuilder } from '@angular/forms';
-import { MatDialog } from '@angular/material'
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, FormBuilder } from "@angular/forms";
+import { MatDialog } from "@angular/material";
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './register.component.html',
-    styleUrls: ['./register.component.css']
+  selector: "app-login",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 
 export class RegisterComponent implements OnInit {
-    registerForm: FormGroup;
-    public passwordForm: FormGroup;
+  public registerForm: FormGroup = this.fb.group({
+    email: new FormControl("", [ Validators.required, Validators.email, Validators.maxLength(255)]),
+    passwords: this.fb.group({
+      password: new FormControl("", [Validators.required]),
+      confirmPassword: new FormControl("", [Validators.required]),
+    }, {
+        validator: this._matchPassword,
+      }),
+    phone: new FormControl("", [Validators.required]),
+  });
 
-    constructor(private router: Router, private fb: FormBuilder) {
-        this._createForm();
+  constructor(private router: Router, private fb: FormBuilder) {
+
+  }
+
+  public ngOnInit() {
+  }
+  public login(): void {
+
+  }
+
+  private _matchPassword(AC: AbstractControl): any {
+    const password = AC.get("password").value; // to get value in input tag
+    const confirmPassword = AC.get("confirmPassword").value; // to get value in input tag
+    if (password !== confirmPassword) {
+      console.log("false");
+      AC.get("confirmPassword").setErrors({ MatchPassword: true });
+    } else {
+      console.log("true");
+      return null;
     }
-
-    ngOnInit() {
-    }
-    login(): void {
-
-    }
-
-
-    private _createForm() {
-        this.passwordForm = this.fb.group({
-            password: ['', Validators.required],
-            confirmPassword: ['', Validators.required]
-          }, {
-            validator: this._matchPassword
-          });
-
-        this.registerForm = this.fb.group({
-            email: new FormControl(``, [Validators.required, Validators.email, Validators.maxLength(255)]),
-            passwordForm: this.passwordForm,
-            phone: new FormControl('', [Validators.required]),
-        })
-    }
-
-
-    private _matchPassword(AC: AbstractControl): any {
-        let password = AC.get('password').value; // to get value in input tag
-        let confirmPassword = AC.get('confirmPassword').value; // to get value in input tag
-         if(password != confirmPassword) {
-             console.log('false');
-             AC.get('confirmPassword').setErrors( {MatchPassword: true} )
-         } else {
-             console.log('true');
-             return null
-         }
-     }
-
+  }
 }
