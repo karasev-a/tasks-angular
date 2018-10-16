2a73b735809711ccfa504cc1c4628c1da9f8ff9e
karasev-a <karasev.avk@gmail.com>
2018-10-16 15:42:00 +0300

    change validation in register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, FormBuilder } from "@angular/forms";
-import { MatDialog } from "@angular/material";
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import { MyErrorStateMatcher } from '../error/error-state-matcher';
+
+
 
 @Component({
   selector: "app-login",
@@ -10,36 +12,36 @@ import { MatDialog } from "@angular/material";
 })
 
 export class RegisterComponent implements OnInit {
-  public registerForm: FormGroup = this.fb.group({
-    email: new FormControl("", [ Validators.required, Validators.email, Validators.maxLength(255)]),
-    passwords: this.fb.group({
-      password: new FormControl("", [Validators.required]),
-      confirmPassword: new FormControl("", [Validators.required]),
-    }, {
-        validator: this._matchPassword,
-      }),
-    phone: new FormControl("", [Validators.required]),
-  });
+  matcher = new MyErrorStateMatcher();
+
+  public registerForm: FormGroup;
 
   constructor(private router: Router, private fb: FormBuilder) {
 
   }
 
   public ngOnInit() {
+    this.registerForm = this.fb.group({
+      email: new FormControl("", [Validators.required, Validators.email, Validators.maxLength(255)]),
+      passwords: this.fb.group({
+        password: new FormControl("", [Validators.required]),
+        confirmPassword: new FormControl("", [Validators.required]),
+      }, {
+          validator: this.checkPass
+
+        }),
+      phone: new FormControl("", [Validators.required]),
+    });
+
   }
   public login(): void {
 
   }
 
-  private _matchPassword(AC: AbstractControl): any {
-    const password = AC.get("password").value; // to get value in input tag
-    const confirmPassword = AC.get("confirmPassword").value; // to get value in input tag
-    if (password !== confirmPassword) {
-      console.log("false");
-      AC.get("confirmPassword").setErrors({ MatchPassword: true });
-    } else {
-      console.log("true");
-      return null;
-    }
+  private checkPass(formGroupPass: FormGroup) {
+    const password = formGroupPass.controls.password.value
+    const confirmPassword = formGroupPass.controls.confirmPassword.value;
+    return (password !== confirmPassword) ? { matchPassword: true } : null;
   }
+
 }
