dee7d0c0bd05a245b893a39fda00d2ee129e9dc2
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-10-18 16:33:31 +0300

    small tslint fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CustomMaterialModule } from './core/material.module';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from "./+login/login.component"
-import { RegisterComponent } from "./register/register.component"
+import { LoginComponent } from './+login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth.guard';
-import {AuthInterceptorService} from './auth/auth-interceptor.service' 
-import {AlertComponent} from './alert/alert.component'
+import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    AlertComponent
+    AlertComponent,
 
   ],
   imports: [
@@ -40,10 +39,10 @@ import {AlertComponent} from './alert/alert.component'
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
-      multi: true
+      multi: true,
     },
-    AuthGuard
+    AuthGuard,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
