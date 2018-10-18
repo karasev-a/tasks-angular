5af93e7204bf2ca8b4e9dae95e834db5b7b68db4
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-10-18 17:05:57 +0300

    other tslint fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import {LoginComponent} from "./+login/login.component";
-import {RegisterComponent} from "./register/register.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './+login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes = [
-  {path : "", component : LoginComponent},
-  { path: "login", component: LoginComponent },
-  {path: "registration", component: RegisterComponent},
+  { path : '', component : LoginComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegisterComponent},
 ];
 
 @NgModule({
