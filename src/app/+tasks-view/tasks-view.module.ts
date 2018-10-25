27559837d9faeddf1c4d75ae6381d3dc6e2407fc
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-10-25 12:39:28 +0300

    rename modules

--- src/app/+tasks-view/tasks-view.module.ts
+++ src/app/+tasks-view/tasks-view.module.ts
@@ -9,7 +9,7 @@ import { CategoriesService } from './servises/categories.service';
 import { TasksService } from './servises/tasks.service';
 import { TaskComponent } from './task/task.component';
 import { TasksViewComponent } from './tasks-view.component';
-import { CategoriesRouting } from './categories-routing.module';
+import { CategoriesRouting } from './tasks-view-routing.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { TasksResolverService } from './servises/tasks-resolver.service';
 
