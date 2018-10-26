5d0d182217425b579a3b1316324376f718f8f167
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-10-26 11:24:21 +0300

    add redirection after login, fix lints

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { AuthService } from '../auth/auth.service';
 import { AlertService } from '../alert/services/alert.service';
 
 @Component({
-    selector: "app-login",
-    templateUrl: "./login.component.html",
-    styleUrls: ["./login.component.css"],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 
 export class LoginComponent implements OnInit {
@@ -21,8 +21,8 @@ export class LoginComponent implements OnInit {
 
     public ngOnInit() {
         this.loginForm = this.fb.group({
-            email: new FormControl("", [Validators.required, Validators.email, Validators.maxLength(255)]),
-            password: new FormControl("", [Validators.required, Validators.maxLength(255)]),
+            email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(255)]),
+            password: new FormControl('', [Validators.required, Validators.maxLength(255)]),
         });
     }
 
@@ -31,6 +31,7 @@ export class LoginComponent implements OnInit {
 
         if (loginData.email && loginData.password) {
             this._authService.login(loginData.email, loginData.password);
+            this.router.navigate(['/categories']);
         }
     }
 }
