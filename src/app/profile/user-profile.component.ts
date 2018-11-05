ba12279478bb36b7e67f4cb5370b52bf6b09cd43
Yegor Dyomin <2048tb@gmail.com>
2018-11-05 08:51:45 +0200

    add form somefields

--- src/app/profile/user-profile.component.ts
+++ src/app/profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-user-profile',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class UserProfileComponent implements OnInit {
-    constructor() { }
+    public profileEditForm: FormGroup;
+    constructor(private _fb: FormBuilder) { }
 
     ngOnInit() {
-
+        this.profileEditForm = this._fb.group({
+            firstName: new FormControl(``, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
+            lastName: new FormControl(''),
+            email: new FormControl('', [Validators.required, Validators.min(1), Validators.max(5)]),
+        });
     }
 }
