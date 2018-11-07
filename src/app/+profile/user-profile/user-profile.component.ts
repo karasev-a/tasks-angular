5e6065bb3cee00c5ced5fb3d5f8e215d209e81bf
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-11-07 10:55:58 +0200

    add handler  for cancel button

--- src/app/+profile/user-profile/user-profile.component.ts
+++ src/app/+profile/user-profile/user-profile.component.ts
@@ -19,26 +19,30 @@ export class UserProfileComponent implements OnInit {
         this._route.data.subscribe((data: { user: IUser }) => {
             this._user = data.user;
         });
-
         this.profileEditForm = this._fb.group({
             firstName: new FormControl(this._user.firstName, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
-            lastName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
-            email: new FormControl('', [Validators.required, Validators.min(1), Validators.max(255)]),
+            lastName: new FormControl(this._user.lastName, [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
+            email: new FormControl(this._user.email, [Validators.required, Validators.min(1), Validators.max(255)]),
             oldPswd: new FormControl('', [Validators.required, Validators.min(4), Validators.max(255)]),
             newPswd: new FormControl('', [Validators.required, Validators.min(4), Validators.max(255)]),
             cPswd: new FormControl('', [Validators.required, Validators.min(4), Validators.max(255)]),
-
         });
+        // this.profileEditForm.patchValue([this._user.firstName, this._user.lastName, this._user.email]);
     }
     onPswd() {
         this._passState = true;
     }
     onCancel() {
         this._passState = false;
-        this.profileEditForm.setValue({
-            firstname: this._user.firstName,
+        // this.profileEditForm.patchValue({
+        //     firstname:
+        //     lastName: this._user.lastName,
+        //     email: this._user.email,
+        // });
+        this.profileEditForm.patchValue({
+            firstName: this._user.firstName,
             lastName: this._user.lastName,
             email: this._user.email,
-        });
+          });
     }
 }
