b275e2c6e48a0ca5a388b91b822b76a1b416fa0d
karasev-a <karasev.avk@gmail.com>
2018-11-13 18:19:27 +0200

    added empty component for tasks of admin

---
+++ src/app/+tasks/tasks-list-admin/tasks-list-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tasks-list-component',
+    templateUrl: './tasks-list-admin.component.html',
+    styleUrls: ['./tasks-list-admin.component.css'],
+
+})
+export class TasksListAdminComponent {
+   
+}
