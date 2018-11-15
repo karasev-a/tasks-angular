29ce5223a5765ed21514dd0afb6ff8063fe02a36
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-11-15 18:47:42 +0200

    add new categories

--- src/app/+profile/admin-categories/A-addNew-category.component.ts
+++ src/app/+profile/admin-categories/A-addNew-category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { CategoriesService } from '../../+categories/services/categories.service';
 
 @Component({
     selector: 'app-add-categories',
@@ -9,10 +10,13 @@ import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms'
 
 export class AAddNewCategoryComponent implements OnInit {
     private newCategoryForm: FormGroup;
-    constructor(private _fb: FormBuilder) { }
+    constructor(private _fb: FormBuilder, private categoriesService: CategoriesService) { }
     ngOnInit() {
         this.newCategoryForm = this._fb.group({
             categoryName: new FormControl( '', [Validators.required, Validators.minLength(3), Validators.maxLength(255)]),
         });
+    }
+    onAdd() {
+       this.categoriesService.sendNewCategory({ name: this.newCategoryForm.value.categoryName } as ICategory).subscribe();
     }
 }
