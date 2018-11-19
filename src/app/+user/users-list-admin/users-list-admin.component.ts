0b31ba56766190b407e2985ef0417bc16675f799
karasev-a <karasev.avk@gmail.com>
2018-11-19 17:55:22 +0200

    added show count task by status

--- src/app/+user/users-list-admin/users-list-admin.component.ts
+++ src/app/+user/users-list-admin/users-list-admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { IUser } from '../models/user.model';
 import { MatTableDataSource } from '@angular/material';
 import { UserService } from '../models/services/user.service';
-import { Roles } from '../../+tasks/models/task';
+import { Roles, Statuses } from '../../+tasks/models/task';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -12,12 +12,16 @@ import { FormControl } from '@angular/forms';
 })
 
 export class UsersListAdminComponent implements OnInit {
-    public displayedColumns: string[] = ['firstName', 'lastName', 'createdAt', 'onReview', 'open', 'pending', 'done', 'decline', 'role', 'actions'];
+    public displayedColumns: string[] = ['firstName', 'lastName', 'createdAt', 'allTasks', 'onReview', 'open', 'pending', 'done', 'decline', 'role', 'actions'];
     public userDataSource = new MatTableDataSource<IUser>();
     public roles = Roles;
     public keysOfRoles: string[];
     public userId: number;
     public currentUser: IUser;
+    public previousRole: string;
+    public currentRole: string;
+    public countSelectedRole: number = 0;
+    public statuses = Statuses;
 
     constructor(
         // private _router: Router,
@@ -34,22 +38,42 @@ export class UsersListAdminComponent implements OnInit {
     }
 
     public onSelectRole(value: string, user: IUser, id: number) {
+        this.countSelectedRole++;
         this.userId = id;
+        if ( this.countSelectedRole < 2) {
+            this.previousRole = user.roleId;
+        }
+        
+
+        // this.currentUser = Object.assign({}, user);
+        
         user.roleId = value;
-        this.currentUser = user;
+        
+       
+        // this.currentRole = value;
 
     }
 
     public saveUser(user: any) {
+
+        // user.roleId = this.currentRole;
+
         if (user.Tasks) {
             delete user.Tasks;
         }
         this._usersService.updateUserRole(user).subscribe();
         this.userId = 0;
+        this.countSelectedRole = 0;
     }
 
-    public cancelEdit() {
+    public cancelEdit(user: IUser) {
+        user.roleId = this.previousRole;
+        this.countSelectedRole = 0;
+        
+        // user.roleId = this.currentUser.roleId;
+        
         this.userId = 0;
+
     }
 
 }
