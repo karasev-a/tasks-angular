4a8372e4128a4a32ca436d2cd1e2946d0a3243c3
Yegor Dyomin <yegor.dyomin@techs.com.ua>
2018-11-20 18:01:43 +0200

    remove any

--- src/app/dialogs/decline-dialog/decline-task-dialog.component.ts
+++ src/app/dialogs/decline-dialog/decline-task-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material';
 import { TasksService } from '../../+tasks/servises/tasks.service';
 import { ITask, Statuses } from '../../+tasks/models/task';
-
+import { ITaskView } from '../models/TaskView';
 
 @Component({
     selector: 'app-decline-task-dialog',
@@ -12,18 +12,18 @@ import { ITask, Statuses } from '../../+tasks/models/task';
 export class DeclineTaskDialogComponent {
 
     constructor(public dialogRef: MatDialogRef<DeclineTaskDialogComponent>,
-                @Inject(MAT_DIALOG_DATA) public data: any, private _tasksService: TasksService) { }
+                @Inject(MAT_DIALOG_DATA) public data: ITaskView, private _tasksService: TasksService) { }
 
     public onNoClick(): void {
         this.dialogRef.close();
     }
 
     public confirmDecline(): void {
-        this.data.task.status = Statuses.Decline;
-        const tmpTask = this.data.task;
+        this.data.status = Statuses.Decline;
+        const tmpTask = this.data;
         delete tmpTask.Category;
         delete tmpTask.firstLastName;
-        this._tasksService.updateTask(this.data.task.id, this.data.task).subscribe(result => result);
+        this._tasksService.updateTask(this.data.id, this.data).subscribe(result => result);
     }
 
 }
